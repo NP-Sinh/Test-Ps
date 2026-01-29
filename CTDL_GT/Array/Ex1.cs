ddef0964937b39605a42e41da6095a90e3b5bc96
NP-Sinh <npsinh6103@gmail.com>
2026-01-29 18:46:39 +0700

    update

--- CTDL_GT/Array/Ex1.cs
+++ CTDL_GT/Array/Ex1.cs
@@ -6,9 +6,9 @@ namespace CTDL_GT.Array
 {
     internal class Ex1
     {
+        static int[] array = { 4, 90, 32, 432, 636, 6346, -1235, 90 };
         public static void TimMaxArray()
-        {
-            int[] array = { 4, 90, 32, 432, 636, 6346, -1235, 875 };
+        { 
             int max = array[0];
             for (int i = 1; i < array.Length; i++)
             {
@@ -18,6 +18,50 @@ namespace CTDL_GT.Array
                 }
             }
             Console.WriteLine("Phần tử lớn nhất trong mảng là: " + max);
+        }
+        public static void TimMinArray()
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                min = Math.Min(min, array[i]);
+            }
+            Console.WriteLine("Phần tử nhỏ nhất trong mảng là: " + min);
+        }
+        public static void XoaPhanTu()
+        {
+            int n = 3;
+            int al = array.Length;
+            for (int i = n; i < al -1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            --al;
+            for (int i = 0; i < al; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+        public static void TimPhanTuTrungLap()
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        Console.WriteLine("Phần tử trùng lặp là: " + array[i]);
+
+
+
+
+
+
+
+
+                    }
+                }
+            }
         }
     }
 }
--- CTDL_GT/Program.cs
+++ CTDL_GT/Program.cs
@@ -1,3 +1,6 @@
 ﻿using CTDL_GT.Array;
 
-Ex1.TimMaxArray();
+//Ex1.TimMaxArray();
+//Ex1.TimMinArray();
+//Ex1.XoaPhanTu();
+Ex1.TimPhanTuTrungLap();
